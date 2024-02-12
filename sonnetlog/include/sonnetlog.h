b38d74ca2b2d312601279021692bc60488afa7d1
AbsolutionA <157630438+AbsolutionA@users.noreply.github.com>
2024-02-12 13:44:51 +0100

    Added a max size input
    Will now take a maximum size of the log file and when reached will clear the file.
    It will only clear at the beginning when starting the program, so when writing to it, it won't delete the logs after surpassing the maximum size,
    only after starting the program again.

--- sonnetlog/include/sonnetlog.h
+++ sonnetlog/include/sonnetlog.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define SONNET_LOG_FILE_MAX_SIZE_DEFAULT 0xA00000 // 10 MB
+
 
 /** @brief Will print a custom error given to this preprocessor on to stderr and a .txt file that has been set in the sonnet_log_set_file_location() function. */
 #define SERROR(...) do { \
@@ -53,8 +55,9 @@
     } \
 } while(0)
 
-/** @brief Set the file location and name here, doesn't matter if the file already exists, this function must always be called first. */
-extern void sonnet_log_set_file_location_and_name(const char *file_location);
+/** @brief Set the file location and name here, doesn't matter if the file already exists, this function must always be called first.
+ *  And specify the maximum file size for the log file. Use SONNET_LOG_FILE_MAX_SIZE_DEFAULT for the default value "10 MB" */
+extern void sonnet_log_set_file_info(const char *file_location_and_name, long file_max_size);
 
 /** @brief Will print a custom message with a custom inputed tag name given to this function on to stderr and 
  * a .txt file that has been set in the sonnet_log_set_file_location() function and only if the log_on_or_off argument is 1. */
--- sonnetlog/src/sonnetlog.c
+++ sonnetlog/src/sonnetlog.c
@@ -4,25 +4,60 @@
 #include <time.h>
 #include "../include/sonnetlog.h"
 
-static char* sonnet_log_file_location = NULL;
+static char*    sonnet_log_file_location = NULL;
+static long     sonnet_log_max_size = 0;
 
-inline void sonnet_log_set_file_location_and_name(const char *file_location_and_name) {
+inline void sonnet_log_set_file_info(const char *file_location_and_name, long file_max_size) {
     sonnet_log_file_location = (char *)file_location_and_name;
     if (sonnet_log_file_location == NULL) {
         fprintf(stderr, "Warning: File location not given!\n");
         return;
     }
+
+    if(file_max_size > 0) {
+        sonnet_log_max_size = file_max_size;
+    } else {
+        fprintf(stderr, "Error: The maximum size of the log file is not greater then 0.\n");
+    }
+}
+
+long sonnet_log_get_file_size(const char* file_name) {
+    FILE *file = fopen(file_name, "r");
+    if (!file) {
+        fprintf(stderr, "Error: Failed to open log file to get file size: '%s': %s\n", file_name, strerror(errno));
+        return -1;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    if (file_size == -1) {
+        fprintf(stderr, "Error: Failed to get file size: %s\n", strerror(errno));
+    }
+    fclose(file);
+    return file_size;
 }
 
 inline void sonnet_log(const char *msg, const char *tag_name, const int log_on_or_off) {
-
     static size_t i = 0;
+
+    long log_size = sonnet_log_get_file_size(sonnet_log_file_location);
+    if (log_size > sonnet_log_max_size && i == 0) {
+        FILE *file = fopen(sonnet_log_file_location, "w");
+        if (file == NULL) {
+            fprintf(stderr, "Error: Failed to open log file to clear: %s\n", strerror(errno));
+            return;
+        }
+        fclose(file);
+    }
+
+    
     
     FILE *file = fopen(sonnet_log_file_location, "a");
     if (!file) {
         fprintf(stderr, "Error: Failed to open log file: %s\n", strerror(errno));
         return;
     }
+
 
     if (i == 0 && log_on_or_off) {
         fprintf(file, "[New Session] ----------------------------------------------------------------------------------------\n");
